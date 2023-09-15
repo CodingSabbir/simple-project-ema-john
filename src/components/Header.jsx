ac4617dd1509610db57cf28c0cf57fd9da6a67a3
sabbir <mdsabbirrahmanm740@gmail.com>
2023-09-15 16:25:25 +0600

    card data show

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import logo from '../images/logo.png'
 
 const Header = () => {
+    
     return (
-        <div>
-            <img src={logo} alt="" />
-        </div>
+        <>
+           <div>
+                <div className='flex justify-center p-4'>
+                    <img className='w-96 ' src={logo} alt="" />
+                </div>
+                <nav className='bg-black  text-white flex gap-5 md:pl-10'>
+                   <a className='hover:bg-slate-400 p-3' href="/shop">Shop</a>
+                   <a className='hover:bg-slate-400 p-3' href="/order">Order  Review</a>
+                   <a className='hover:bg-slate-400 p-3' href="/manage"> Manage Inventory here</a>
+                </nav>
+           </div>
+        </>
     );
 };
 
